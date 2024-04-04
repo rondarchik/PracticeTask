cef01e3ced3a64d029b4badddd36dbf828c86045
rondarchik <dovgun-viktoriya@mail.ru>
2024-04-04 12:48:03 +0300

    fix mapping in credits

--- back/web-api/src/main/java/org/system/creditmanagementsystem/controller/CreditController.java
+++ back/web-api/src/main/java/org/system/creditmanagementsystem/controller/CreditController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/credit")
+@RequestMapping("/api/credits")
 public class CreditController {
     private final CreditService creditService;
 
