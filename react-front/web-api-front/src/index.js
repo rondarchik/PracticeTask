070655b8b48917bd3de394eef6f3f3771506b131
rondarchik <dovgun-viktoriya@mail.ru>
2024-04-11 15:12:40 +0300

    update cross origins

--- react-front/web-api-front/src/index.js
+++ react-front/web-api-front/src/index.js
@@ -4,10 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'semantic-ui-css/semantic.min.css'
-import axios from 'axios';
-
-axios.defaults.baseURL = 'http://localhost:8080';
+import 'semantic-ui-css/semantic.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,7 +13,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
